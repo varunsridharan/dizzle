5c5f2f5c7c5c04288003542f09d77abb6bc91660
Varun <varunsridharan23@gmail.com>
2020-09-21 20:40:27 +0530

    Added instanceID var.

--- src/core.js
+++ src/core.js
@@ -2,6 +2,8 @@ function Dizzle( selector, context ) {
 	return Dizzle.find( selector, context );
 }
 
+Dizzle.instanceID = `dizzle${1 * new Date()}`;
+
 Dizzle.err = msg => {
 	throw new Error( msg );
 };
