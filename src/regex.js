3ce87d56706cc1d6fc74f036ca51b7ba57b49bc4
Varun <varunsridharan23@gmail.com>
2020-09-21 07:29:35 +0530

    Added Regex Var.

--- src/regex.js
+++ src/regex.js
@@ -1,8 +1,10 @@
-export const reName      = /^[^\\]?(?:\\(?:[\da-f]{1,6}\s?|.)|[\w\-\u00b0-\uFFFF])+/,
-			 reEscape    = /\\([\da-f]{1,6}\s?|(\s)|.)/gi,
+export const reName          = /^[^\\]?(?:\\(?:[\da-f]{1,6}\s?|.)|[\w\-\u00b0-\uFFFF])+/,
+			 reEscape        = /\\([\da-f]{1,6}\s?|(\s)|.)/gi,
 			 // Modified version of https://github.com/jquery/sizzle/blob/master/src/sizzle.js#L87
-			 reAttr      = /^\s*((?:\\.|[\w\u00b0-\uFFFF-])+)\s*(?:(\S?)=\s*(?:(['"])([^]*?)\3|(#?(?:\\.|[\w\u00b0-\uFFFF-])*)|)|)\s*(i)?\]/,
+			 reAttr          = /^\s*((?:\\.|[\w\u00b0-\uFFFF-])+)\s*(?:(\S?)=\s*(?:(['"])([^]*?)\3|(#?(?:\\.|[\w\u00b0-\uFFFF-])*)|)|)\s*(i)?\]/,
 			 // Easily-parseable/retrievable ID or TAG or CLASS selectors
-			 rquickExpr  = /^(?:#([\w-]+)|(\w+)|\.([\w-]+))$/,
-			 whitespace  = "[\\x20\\t\\r\\n\\f]",
-			 rwhitespace = new RegExp( whitespace + "+", "g" );
+			 rquickExpr      = /^(?:#([\w-]+)|(\w+)|\.([\w-]+))$/,
+			 whitespace      = "[\\x20\\t\\r\\n\\f]",
+			 rwhitespace     = new RegExp( whitespace + "+", "g" ),
+			 // Below Regex is used to find any issues with string such as using / or \ - _ (Any Special Char Thats Needs To Be Escaped)
+			 rfindEscapeChar = /[-[\]{}()*+?.,\\^$|#\s]/g;
