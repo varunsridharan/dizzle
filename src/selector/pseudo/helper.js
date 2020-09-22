6dafd6ac9034fe695e7fbdb5bb33dd7ccd8e0a61
Varun <varunsridharan23@gmail.com>
2020-09-22 12:46:10 +0530

    Fixed Token Argument Issue.

--- src/selector/pseudo/helper.js
+++ src/selector/pseudo/helper.js
@@ -1,11 +1,10 @@
 import { markFunction } from "../../helper";
 
 export function createPositionalPseudo( fn ) {
-	return markFunction( ( elements, argument ) => {
-		argument         = +argument;
-		let results      = fn( [], elements.length, argument );
+	return markFunction( ( elements, token ) => {
+		token.data       = +token.data;
+		let results      = fn( [], elements.length, token );
 		let currentIndex = results.shift();
-		console.log(results);
 		// Match elements found at the specified indexes
 		return elements.filter( ( el, index ) => {
 			if( currentIndex === index ) {
