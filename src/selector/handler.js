8cf74e265c284eacf7f0c5401ea3baec2d52dca3
Varun <varunsridharan23@gmail.com>
2020-09-22 07:40:49 +0530

    renamed contxt to context

--- src/selector/handler.js
+++ src/selector/handler.js
@@ -1,7 +1,7 @@
 import _filter from "../vars/_filter";
 
-export function attrHandler( data, contxt ) {
-	return _filter.call( contxt, ( elem ) => {
+export function attrHandler( data, context ) {
+	return _filter.call( context, ( elem ) => {
 		let value = elem.getAttribute( data.id );
 		switch( data.action ) {
 			case '!':
