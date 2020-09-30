3df17f730130203b3b468338ea463e39ddf22faf
Varun <varunsridharan23@gmail.com>
2020-09-30 17:10:00 +0530

    Migrated To Sofy Builder

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
-import babel from 'rollup-plugin-babel';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import { uglify } from 'rollup-plugin-uglify';
 import filesize from 'rollup-plugin-filesize';
 import visualizer from 'rollup-plugin-visualizer';
