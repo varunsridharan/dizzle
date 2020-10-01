52c65170b536aea24c01a9c6e3f51e2471aa412c
Varun <varunsridharan23@gmail.com>
2020-10-01 14:48:36 +0530

    Removed unused var

--- src/vars/index.js
+++ src/vars/index.js
@@ -1,3 +1,1 @@
-export var CombinatorTypes           = [ '>', '<', '~', '+' ];
-export var BrowserSupportedOperators = [ '=' ];
-export var BrowserSupportedPseudo    = [ 'first-child' ];
+export var CombinatorTypes = [ '>', '<', '~', '+' ];
