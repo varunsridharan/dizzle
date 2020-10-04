93dd143e7081a32849ad8747a45c4e4fabeaa33a
Varun <varunsridharan23@gmail.com>
2020-10-04 09:12:37 +0530

    Added Error Function.

--- src/dizzlecore.js
+++ src/dizzlecore.js
@@ -2,10 +2,11 @@ function DizzleCore( selector, context ) {
 	return DizzleCore.find( selector, context );
 }
 
-DizzleCore.instanceID = 'dizzle' + ( 1 * new Date() );
-
-DizzleCore.err = msg => {
+export function err( msg ) {
 	throw new Error( msg );
-};
+}
+
+DizzleCore.instanceID = 'dizzle' + ( 1 * new Date() );
+DizzleCore.err        = err;
 
 export default DizzleCore;
