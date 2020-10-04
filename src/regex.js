baf09f9dd62f4695e6e7b304c71bbc468d9e2735
Varun <varunsridharan23@gmail.com>
2020-10-04 09:12:53 +0530

    Updated reAttr

--- src/regex.js
+++ src/regex.js
@@ -1,7 +1,7 @@
 export const reName          = /^[^\\]?(?:\\(?:[\da-f]{1,6}\s?|.)|[\w\-\u00b0-\uFFFF])+/,
 			 reEscape        = /\\([\da-f]{1,6}\s?|(\s)|.)/gi,
 			 // Modified version of https://github.com/jquery/sizzle/blob/master/src/sizzle.js#L87
-			 reAttr          = /^\s*((?:\\.|[\w\u00b0-\uFFFF-])+)\s*(?:(\S?)=\s*(?:(['"])([^]*?)\3|(#?(?:\\.|[\w\u00b0-\uFFFF-])*)|)|)\s*(i)?\]/,
+			 reAttr          = /^\s*((?:\\.|[\w\u00b0-\uFFFF-])+)\s*(?:(\S?)=\s*(?:(['"])((?:[^\\]|\\[^])*?)\3|(#?(?:\\.|[\w\u00b0-\uFFFF-])*)|)|)\s*(i)?\]/,
 			 // Easily-parseable/retrievable ID or TAG or CLASS selectors
 			 rquickExpr      = /^(?:#([\w-]+)|(\w+)|\.([\w-]+))$/,
 			 whitespace      = '[\\x20\\t\\r\\n\\f]',
